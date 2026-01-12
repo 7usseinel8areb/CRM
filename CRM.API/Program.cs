3734867492ca4eeb923c4443c21f63ae7ca0630b
7usseinel8areb <halghryb287@gmail.com>
2026-01-12 22:17:52 +0300

    Add identity

--- CRM.API/Program.cs
+++ CRM.API/Program.cs
@@ -1,6 +1,14 @@
 using CRM.API.Extensions;
+using CRM.API.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 
