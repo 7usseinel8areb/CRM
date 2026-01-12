54f85011df3f72770d0fbbf0b1bcf2b0b418559d
7usseinel8areb <halghryb287@gmail.com>
2026-01-12 23:41:12 +0300

    Seperate SQL Connection from API Layer make extension

--- CRM.API/Program.cs
+++ CRM.API/Program.cs
@@ -1,33 +1,31 @@
 using CRM.API.Extensions;
-using CRM.DataAccess.Data;
+using CRM.DataAccess.Extensions;
 using CRM.Model.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found."); ;
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") 
+        ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found."); ;
 
 // No Need for default identity in API  
 //builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
-// Add services to the container.
+builder.Services.AddSQLServerConnection(connectionString);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
 builder.Services.AddSwaggerDocumentation(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    //app.UseDeveloperExceptionPage();
+    app.UseDeveloperExceptionPage();
 
     app.UseSwaggerDocumentation(app.Configuration);
-
-    //app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
---
+++ CRM.DataAccess/Extensions/SQLConnectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CRM.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.DataAccess.Extensions
+{
+    public static class SQLConnectionExtensions
+    {
+        public static IServiceCollection AddSQLServerConnection(this IServiceCollection service, string connectionString)
+        {
+            service.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            return service;
+        }
+    }
+}
