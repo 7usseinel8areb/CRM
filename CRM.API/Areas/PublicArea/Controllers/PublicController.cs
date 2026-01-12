29f93900f62deb85f0d0a1514c320d6367ca36c5
7usseinel8areb <halghryb287@gmail.com>
2026-01-12 21:17:15 +0300

    Add api project with areas

---
+++ CRM.API/Areas/PublicArea/Controllers/PublicController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace CRM.API.Areas.PublicArea.Controllers
+{
+    [Area("PublicArea")]
+    [Route("api/[area]/[controller]")]
+    [DisplayName("Public Controller")]
+    [ApiController]
+    public class PublicController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is a public endpoint.");
+        }   
+    }
+}
