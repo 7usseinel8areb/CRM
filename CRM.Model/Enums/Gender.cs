235c0e99fbc21e0ea6958674a52a53e8d348df0c
7usseinel8areb <halghryb287@gmail.com>
2026-01-12 23:20:11 +0300

    Update user model

---
+++ CRM.Model/Enums/Gender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace CRM.Model.Enums;
+
+public enum Gender
+{
+    [EnumMember(Value = "Male")]
+    Male,
+    [EnumMember(Value = "Female")]
+    Female,
+    [EnumMember(Value = "Other")]
+    Other
+}
